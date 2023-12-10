82d02c43ffaea1d78c98c4e726be197516763398
Joey Lim <joey@JL-Macbook-Pro.lan>
2023-12-10 15:34:45 +0800

    test gnews for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import "./App.css";
 function App() {
   const [apiCall, setApiCall] = useState({});
 
-  const apiKey = "a02ce2a2496d4179b997289d54029c0f";
+  const apiKey = "1331e8039126b4d683d96e90caaf55de";
 
   async function fetchAPI() {
     const response = await fetch(
-      `https://newsapi.org/v2/everything?q=Gaza&from=2023-12-08&to=2023-12-08&sortBy=popularity&apiKey=${apiKey}`
+      `https://gnews.io/api/v4/top-headlines?country=sg&apikey=${apiKey}`
     );
     const result = await response.json();
     console.log(result);
@@ -20,12 +20,17 @@ function App() {
   return (
     <>
       <h1>Test APIs</h1>
-      <button onClick={handleClick}>Click here to fetch APIs</button>
+      <button name="UV" onClick={handleClick}>
+        Click here to fetch UV data
+      </button>
+      <button name="2.5" onClick={handleClick}>
+        Click here to fetch PM2.5 data
+      </button>
       <div name="content">
-        {/* <pre>{JSON.stringify(apiCall, null, 2)}</pre> */}
+        <pre>{JSON.stringify(apiCall, null, 2)}</pre>
         <p>
           {apiCall?.articles?.map(
-            (item) => `${item.source.name} + ${item.title}`
+            (item) => `${item.title} + ${item.source.name}`
           )}
         </p>
       </div>
