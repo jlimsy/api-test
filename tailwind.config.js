68c3acfbbfa5f1048b5ead55b53dd6e9afeaba67
jlimsy <limsy.joey@gmail.com>
2024-01-08 22:50:24 +0800

    added content in tailwind.config to see if navbar works on vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
